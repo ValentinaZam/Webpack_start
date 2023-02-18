38779fba936d1dca6631b6d8e86c6fbc5319a435
Валентина <abc99-33@yandex.ru>
2023-02-18 17:25:25 +0300

    Webpack plugins

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import './index.css';
+import Icon from './assets/img.png';
+
+const jsImage = document.createElement('img');
+jsImage.className = 'image'
+jsImage.src = Icon;
+
+document.body.append(jsImage);
+
 console.log('Hello World!');
--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,38 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'my-bundle.js',
     clean: true,
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ],
+  },
+  devServer: {
+    port: 8080,
+    open: true,
+  },
 };
